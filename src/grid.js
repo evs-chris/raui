a52ad5f082021e90674f4b8df230383cc7d80f0e
Chris Reeves <chris@ev-soft.net>
2018-03-03 23:42:53 -0500

    better grid cascading

--- src/grid.js
+++ src/grid.js
@@ -12,55 +12,72 @@ function sizer() {
 const defaults = {
   tiny: {
     units: [ 2, 3, 4, 5, 6, 8 ],
-    max: '0'
+    max: '0',
+    value: 0
   },
   xsmall: {
-    units: [ 2, 3, 4, 5, 6, 8 ],
+    units: [ 2, 3, 4, 5, 6, 8, 10, 12 ],
     max: '20em',
-    prefix: 'xs'
+    prefix: 'xs',
+    value: 10
   },
   small: {
     units: [ 2, 3, 4, 5, 6, 8, 10, 12 ],
-    max: '36em'
+    max: '36em',
+    value: 20
   },
   medium: {
     units: [ 2, 3, 4, 5, 6, 8, 10, 12 ],
-    max: '48em'
+    max: '48em',
+    value: 30
   },
   large: {
     units: [ 2, 3, 4, 5, 6, 8, 10, 12, 20, 24 ],
-    max: '64em'
+    max: '64em',
+    value: 40
   },
   xlarge: {
     units: [ 2, 3, 4, 5, 6, 8, 10, 12, 20, 24, 32 ],
-    max: '100em'
+    max: '100em',
+    value: 50
   },
   ginormous: {
     units: [ 2, 3, 4, 5, 6, 8, 10, 12, 20, 24, 32, 64 ],
-    max: '150em'
+    max: '150em',
+    value: 60
   }
 }
 
 const regexps = { 'grid grid-root': /\bgrid grid-root\b/g };
 const spaces = /\s+/g;
-export function grid(node, _type, _path) {
+export function grid(node, options) {
   const ctx = this.getContext(node);
   const owner = this;
   let points;
-  let type = _type;
-  let path = _path;
+  let opts = options || {};
+  let breaks;
 
   function resize() {
     let matches = [];
     const size = node.clientWidth;
 
-    if (!type || type === 'class') {
+    if (!opts.type || opts.type === 'class') {
+      let match, max = -1;
       for (const k in points) {
         if (points[k] <= size) {
           regexps[k].lastIndex = -1;
-          if (!regexps[k].test(node.className)) node.className += ` ${k}`;
-        } else node.className = node.className.replace(regexps[k], '').trim();
+          if (points[k] > max) {
+            match = k;
+            max = points[k];
+          }
+        }
+        node.className = node.className.replace(regexps[k], '').trim();
       }
+
+      if (!regexps[match].test(node.className)) node.className += ` ${match}`;
+
+      if (opts.value) ctx.set(opts.value, breaks[match].value);
+      if (opts.name) ctx.set(opts.name, breaks[match].value);
 
       node.className = node.className.replace(spaces, ' ');
     }
@@ -68,7 +85,7 @@ export function grid(node, _type, _path) {
 
   function settings() {
     const s = sizer();
-    const breaks = owner.get('@style.break') || defaults;
+    breaks = owner.get('@style.break') || defaults;
     points = {};
     for (const k in breaks) {
       s.style.width = breaks[k].max;
@@ -85,10 +102,9 @@ export function grid(node, _type, _path) {
   settings();
 
   return {
-    update(_type, _path) {
+    update(options) {
       // TODO: if type changes, undo whatever the original did first
-      type = _type;
-      path = _path;
+      opts = options || {};
       resize();
     },
     teardown() {
@@ -102,24 +118,31 @@ export function grid(node, _type, _path) {
 export function style(data) {
   const defs = data('break') || defaults;
 
-  let out = `.grid > .row > * { position: relative; width: 100%; transition: width 0.2s ease-in-out; }
+  let out = `.row > * { position: relative; width: 100%; transition-duration: 0.2s; transition-timing-function: ease-in-out; transition-property: width, padding, margin; box-sizing: border-box; }
 .grid { display: block; }
-.grid > .row { display: flex; flex-wrap: wrap; }
+.grid > .row { display: flex; flex-wrap: wrap; min-height: fit-content; }
 .grid > .row > .pad { display: flex; flex-direction: column; padding: 0.5em; box-sizing: border-box; }`;
 
   let str;
 
-  Object.keys(defs).forEach(k => {
-    const size = defs[k];
-    const name = size.prefix || k[0];
+  const points = Object.keys(defs).map(k => (defs[k].key = k) && defs[k]);
+  points.sort((l, r) => l.value > r.value ? 1 : l.value < r.value ? -1 : 0);
+  const greater = {};
+  points.reverse().reduce((a, c) => {
+    a.push(c.key);
+    greater[c.key] = a.slice();
+    return a;
+  }, []);
+  points.reverse().forEach(size => {
+    const name = size.prefix || size.key[0];
 
-    out += `\n.${k} > .row > .${name}0 { width: 0; overflow: hidden; }`;
+    out += `\n${greater[size.key].map(s => `.${s} .${name}0`).join(', ')} { width: 0; overflow: hidden; }`;
 
     size.units.forEach(u => {
       for (let i = 1; i < u; i++) {
         str = '' + ((i / u) * 100);
         str = str.substr(0, str.indexOf('.') + 3);
-        out += `\n.${k} > .row >  .${name}${i}-${u} { width: ${str}%; }`;
+        out += `\n${greater[size.key].map(s => `.${s} .${name}${i}-${u}`).join(', ')} { width: ${str}%; }`;
       }
     });
   });
