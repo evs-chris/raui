052aa8277d8b660b4131ef4286459fdb7a351a60
Chris Reeves <evschris@gmail.com>
2018-03-24 12:33:25 -0400

    build: create es directory if it doesn't exist

--- scripts/esm.sh
+++ scripts/esm.sh
@@ -5,6 +5,10 @@ export PATH=./node_modules/.bin:../node_modules/.bin:$PATH
 if [ "$NODE_ENV" != "dev" ]; then
     rollup -c rollup.config.js
     
+    if [ ! -d es ]; then
+        mkdir es
+    fi
+
     ls src/*.ractive.html | while read cmp; do
         echo compiling $cmp ...
         cat $cmp | ractive component | buble --no modules > es/`basename $cmp .ractive.html`.js
