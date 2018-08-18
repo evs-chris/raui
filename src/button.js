e560a49a7f7c0f5ab4ec060729a4355d9c7e0b59
Chris Reeves <evschris@gmail.com>
2018-08-18 19:23:58 -0400

    button: adjust padding

--- src/button.js
+++ src/button.js
@@ -14,7 +14,7 @@ export function button(data) {
       user-select: none;
       border: none;
       border-radius: ${primary.radius || '0.2em'};
-      padding: 0 2rem;
+      padding: 0 1.25rem;
       box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
         0 1px 5px 0 rgba(0, 0, 0, 0.12),
         0 3px 1px -2px rgba(0, 0, 0, 0.2);
