4e191e151e7a9504cb34a36656a2e1728723e6d9
Chris Reeves <evschris@gmail.com>
2018-08-18 19:24:44 -0400

    clickout: defer event registration so that elements don't accidentally catch a click event that happened to show them

--- src/event-clickout.js
+++ src/event-clickout.js
@@ -1,20 +1,31 @@
 export function clickout(node, fire) {
+  let registered = false, torndown = false;
   function handler(ev) {
     let n = ev.target;
     while (n) {
-      if (n === node) return false;
+      if (n === node) return;
       n = n.parentNode;
     }
     fire(ev);
   }
 
-  document.body.addEventListener('click', handler);
-  document.body.addEventListener('touchstop', handler);
+
+  setTimeout(() => {
+    registered = true;
+    if (!torndown) {
+      document.body.addEventListener('click', handler);
+      document.body.addEventListener('touchstop', handler);
+    }
+  });
+  
 
   return {
     teardown() {
-      document.body.removeEventListener('click', handler);
-      document.body.removeEventListener('touchstop', handler);
+      torndown = true;
+      if (registered) {
+        document.body.removeEventListener('click', handler);
+        document.body.removeEventListener('touchstop', handler);
+      }
     }
   }
 }
