66f3dfac73baafb1d7e5567e8b18b4fecc348910
Chris Reeves <evschris@gmail.com>
2018-10-13 18:15:15 -0400

    expand: defer animation start to next frame to avoid weird sizing jumps

--- src/transition-expand.js
+++ src/transition-expand.js
@@ -2,24 +2,29 @@ import globalRegister from './globalRegister';
 
 export function expand(t, params) {
   const p = t.processParams(params, { duration: 200, easing: 'easeInOut' });
-  t.setStyle('overflow', 'hidden');
-  const axis = p.axis === 'x' ? 'width' : 'height';
-  if (t.isIntro || p.intro) {
-    const val = t.getStyle(axis);
-    t.setStyle(axis, 0);
-    t.setStyle('opacity', 0);
-    return t.animateStyle(axis, val, p)
-      .then(() => t.animateStyle('opacity', 1, p))
-      .then(() => {
-        t.setStyle(axis, '');
-        t.setStyle('overflow', '');
-      });
-  } else {
-    t.setStyle(axis, t.getStyle(axis));
-    t.setStyle('opacity', 1);
-    return t.animateStyle('opacity', 0, p)
-      .then(() => t.animateStyle(axis, 0, p));
-  }
+  return new Promise(ok => {
+    // defer execution to allow for grid stuff
+    requestAnimationFrame(() => {
+      t.setStyle('overflow', 'hidden');
+      const axis = p.axis === 'x' ? 'width' : 'height';
+      if (t.isIntro || p.intro) {
+        const val = t.getStyle(axis);
+        t.setStyle(axis, 0);
+        t.setStyle('opacity', 0);
+        ok(t.animateStyle(axis, val, p)
+          .then(() => t.animateStyle('opacity', 1, p))
+          .then(() => {
+            t.setStyle(axis, '');
+            t.setStyle('overflow', '');
+          }));
+      } else {
+        t.setStyle(axis, t.getStyle(axis));
+        t.setStyle('opacity', 1);
+        ok(t.animateStyle('opacity', 0, p)
+          .then(() => t.animateStyle(axis, 0, p)));
+      }
+    });
+  });
 }
 
 export function plugin(opts = {}) {
