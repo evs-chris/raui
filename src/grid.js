b0c3881552535aeb0862638766147e33507ffcde
Chris Reeves <evschris@gmail.com>
2018-11-08 16:04:10 -0500

    grid: make grid display configurable and default to inline-block

--- src/grid.js
+++ src/grid.js
@@ -150,17 +150,17 @@ export function style(data, optDefaults) {
 
     size.units.forEach(u => {
       cols += `
-${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.${name}1, .${s} > ${w}.row > .${name}1, .${s} .${name}-n1, .${s} .row-${name}-n1 > *`).join(', ')).join(', ')} { display: inherit; width: 100%; flex-grow: 0; flex-shrink: 0; }
+${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.${name}1, .${s} > ${w}.row > .${name}1, .${s} .${name}-n1, .${s} .row-${name}-n1 > *`).join(', ')).join(', ')} { display: ${data('raui.grid.display') || 'inline-block'}; width: 100%; flex-grow: 0; flex-shrink: 0; }
 ${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.${name}0, .${s} > ${w}.row > .${name}0, .${s} .${name}-n0, .${s} .row-${name}-n0 > *`).join(', ')).join(', ')} { display: none; flex-grow: 0; flex-shrink: 0; }`;
       rows += `
-${greater[size.key].map(s => `.${s} .row-${name}-n1 > *`).join(', ')} { display: inherit; width: 100%; }
+${greater[size.key].map(s => `.${s} .row-${name}-n1 > *`).join(', ')} { display: ${data('raui.grid.display') || 'inline-block'}; width: 100%; }
 ${greater[size.key].map(s => `.${s} .row-${name}-n0 > *`).join(', ')} { display: none; }`;
 
       for (let i = 1; i < u; i++) {
         str = '' + ((i / u) * 100);
         str = str.substr(0, str.indexOf('.') + 3);
-        rows += `\n${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.row-${name}${i}-${u} > *, .${s} .row-${name}-n${i}-${u} > *, .${s} .row > .${name}-n${i}-${u}`).join(', ')).join(', ')} { display: inherit; width: ${str}%; }`;
-        cols += `\n${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.${name}${i}-${u}, .${s} > ${w}.row > .${name}${i}-${u}, .${s} .${name}-n${i}-${u}, .${s} .row-${name}-n${i}-${u} > *`).join(', ')).join(', ')} { display: inherit; width: ${str}%; flex-grow: 0; flex-shrink: 0; }`;
+        rows += `\n${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.row-${name}${i}-${u} > *, .${s} .row-${name}-n${i}-${u} > *, .${s} .row > .${name}-n${i}-${u}`).join(', ')).join(', ')} { display: ${data('raui.grid.display') || 'inline-block'}; width: ${str}%; }`;
+        cols += `\n${greater[size.key].map(s => wrappers.map(w => `.${s} > ${w}.${name}${i}-${u}, .${s} > ${w}.row > .${name}${i}-${u}, .${s} .${name}-n${i}-${u}, .${s} .row-${name}-n${i}-${u} > *`).join(', ')).join(', ')} { display: ${data('raui.grid.display') || 'inline-block'}; width: ${str}%; flex-grow: 0; flex-shrink: 0; }`;
       }
     });
 
