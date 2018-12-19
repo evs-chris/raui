3cd4d494591655ab3049d09adaeb6e757ff32ec4
Chris Reeves <evschris@gmail.com>
2018-12-18 23:41:45 -0500

    add minified es modules and bump deps

--- scripts/esm.sh
+++ scripts/esm.sh
@@ -12,11 +12,13 @@ if [ "$NODE_ENV" != "dev" ]; then
     ls src/*.ractive.html | while read cmp; do
         echo compiling $cmp ...
         cat $cmp | ractive component | buble --no modules > es/`basename $cmp .ractive.html`.js
+        cat es/`basename $cmp .ractive.html`.js | terser > es/`basename $cmp .ractive.html`.min.js
     done
 
     ls src/*.js | while read hlp; do
         echo compiling $hlp ...
         cat $hlp | buble --no modules > es/`basename $hlp`
+        cat es/`basename $hlp` | terser > es/`basename $hlp .js`.min.js
     done
 else
   rollup -c rollup.config.js &  
