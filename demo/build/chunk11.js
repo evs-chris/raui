320fbeb83c1a014b78ded6477ae99304bbf8b2eb
Chris Reeves <evschris@gmail.com>
2019-01-02 19:56:06 -0500

    build demo

--- demo/build/chunk11.js
+++ demo/build/chunk11.js
@@ -10,6 +10,8 @@ System.register([], function (exports, module) {
         var bind = typeof opts === 'string' ? opts : opts.bind;
         if (typeof bind !== 'string') { return { teardown: function teardown() {} }; }
 
+        var allow = opts.allow || 2;
+
         var ctx = this.getContext(node);
 
         function watch() {
@@ -17,12 +19,12 @@ System.register([], function (exports, module) {
           if (node.scrollHeight > node.clientHeight) { str += 'vscroll'; }
           if (node.scrollWidth > node.clientWidth) { str += (str ? ' ' : '') + 'hscroll'; }
 
-          if (node.scrollTop === 0) { str += ' top'; }
-          else if (node.scrollTop === node.scrollHeight - node.clientHeight) { str += ' bottom'; }
+          if (node.scrollTop <= allow) { str += ' top'; }
+          else if (node.scrollTop >= node.scrollHeight - node.clientHeight - allow) { str += ' bottom'; }
           else { str += ' vmiddle'; }
 
-          if (node.scrollLeft === 0) { str += ' left'; }
-          else if (node.scrollLeft === node.scrollWidth - node.clientWidth) { str += ' right'; }
+          if (node.scrollLeft <= allow) { str += ' left'; }
+          else if (node.scrollLeft >= node.scrollWidth - node.clientWidth - allow) { str += ' right'; }
           else { str += ' hmiddle'; }
 
           ctx.set(bind, str);
