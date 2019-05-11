c49dc8a9f593631413f5f26120192bb0bf697b02
Chris Reeves <evschris@gmail.com>
2019-05-11 03:26:16 -0400

    publish 0.5.16 and build demo

--- demo/build/Table.ractive.html.js
+++ demo/build/Table.ractive.html.js
@@ -1011,7 +1011,7 @@ System.register(['ractive', './chunk7.js', './chunk8.js', './chunk2.js', './chun
         
         this._init = { sets: grabColumns(tpl), partials: {} };
         var columns = this._init.sets.columns;
-        this._init.sets['meta.expand'] = rowEl && rowEl.f.length && rowEl.f;
+        this._init.sets['meta.expand'] = rowEl && rowEl.f && rowEl.f.length;
 
         var sortKey = '[_0._setSort(_1,_2)]';
 
