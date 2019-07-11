b4a810b525cb38396afc6f15d1ab22c7910f092e
Chris Reeves <evschris@gmail.com>
2019-07-11 00:51:24 -0400

    toast: add control handle, partial support for content, and an expandable area

--- src/Toast.d.ts
+++ src/Toast.d.ts
@@ -1,3 +1,5 @@
+import { ContextHelper } from 'ractive';
+
 export interface Options {
   bottom?: boolean;
   left?: boolean;
@@ -5,7 +7,9 @@ export interface Options {
   top?: boolean;
   
   dismissable?: boolean;
+  closeButton?: boolean;
   type?: 'success'|'info'|'warn'|'error'|string;
+  class?: string;
   buttons?: Button[];
 
   timeout?: number;
@@ -17,8 +21,34 @@ export interface Button {
   label: string;
 }
 
+export interface Handle {
+  top?: boolean;
+  bottom?: boolean;
+  left?: boolean;
+  right?: boolean;
+  center?: boolean;
+
+  buttons?: Button[];
+
+  dismissable?: boolean;
+  closeButton?: boolean;
+  type?: 'success'|'info'|'warn'|'error'|string;
+  class?: string;
+
+  message?: string|any[];
+  more?: string|any[];
+  context?: ContextHelper;
+  showMore?: boolean;
+
+  readonly live?: boolean;]
+
+  close(timeout?: number): void;
+  cancelClose(): void;
+  updateButtons(): void;
+}
+
 export interface Toast {
-  toast(message: string, options?: Options): void;
+  toast(message: string, options?: Options): Handle;
 }
 
 export interface PluginOpts extends Options {
