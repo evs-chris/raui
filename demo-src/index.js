c93b4a130cad30b4422a922d242411799ffd1d82
Chris Reeves <evschris@gmail.com>
2020-11-09 03:05:24 -0500

    demo: add debug helpers and window dialog demo

--- demo-src/index.js
+++ demo-src/index.js
@@ -11,3 +11,20 @@ Ractive.use(
 );
 
 var app = window.app = new App({ target: '#target' });
+
+var el;
+document.addEventListener('click', ev => el = ev.target);
+document.addEventListener('focus', ev => el = ev.target);
+
+Object.defineProperty(window, 'C', {
+  get() {
+    return app.getContext(el);
+  }
+});
+
+Object.defineProperty(window, 'R', {
+  get() {
+    const res = window.C;
+    if (res) return res.ractive;
+  }
+});
