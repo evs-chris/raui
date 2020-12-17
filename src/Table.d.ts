364463ab0345306de1e04aac44ef9bdea2b1ced4
Chris Reeves <evschris@gmail.com>
2020-12-17 15:16:48 -0500

    table: add missing typings

--- src/Table.d.ts
+++ src/Table.d.ts
@@ -1,7 +1,23 @@
 import Ractive, { Plugin } from 'ractive';
 
 export class Table<V = any, T extends Table<T> = Table<any>> extends Ractive<T> {
-  readonly selections: V[];
+  selections: V[];
+  selected: V;
+
+  /** Remove any selections. */
+  deselect(): void;
+
+  /** Move the selection up or down the list by the given offset.
+   *
+   * @param offset - the positive (down) or negative (up) offset to move the selection
+   */
+  selectionOffset(offset: number): void;
+
+  /** Move the selection down by one row. */
+  selectionDown(): void;
+
+  /** Move the selection up by one row. */
+  selectionUp(): void;
 }
 
 export interface PluginOpts {
