5cb07c5780feec8ecb2b80d02447ed8be485ce3d
Chris Reeves <evschris@gmail.com>
2021-04-20 14:37:33 -0400

    autofocus: default to delayed focus and add options for immediate and timeout

--- src/autofocus.d.ts
+++ src/autofocus.d.ts
@@ -1,7 +1,9 @@
-import { Decorator, Plugin } from 'ractive';
+import { Plugin } from 'ractive';
 
 export interface PluginOpts {
   name?: string;
+  immediate?: boolean;
+  timeout?: number;
 }
 
 export default function(options?: PluginOpts): Plugin;
--- src/autofocus.js
+++ src/autofocus.js
@@ -2,8 +2,10 @@ function teardown() {}
 
 export default function plugin(options = {}) {
   return function({ instance }) {
-    instance.decorators[options.name || 'autofocus'] = function(node, opts = {}) {
-      node.focus();
+    instance.decorators[options.name || 'autofocus'] = function(node, opts) {
+      const o = Object.assign({}, options, opts);
+      if (o.immediate) node.focus();
+      else setTimeout(() => node.focus(), o.timeout || 250);
       return { teardown };
     };
   };
