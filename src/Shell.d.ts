e2a2fbd4212f6e97d70d63474b5189b387fd8d65
Chris Reeves <evschris@gmail.com>
2021-09-24 17:28:17 -0400

    shell: add shellSize method and add size to resize events

--- src/Shell.d.ts
+++ src/Shell.d.ts
@@ -1,6 +1,7 @@
 import Ractive, { Plugin } from 'ractive';
 
 export class Shell<T extends Shell<T> = Shell<any>> extends Ractive<T> {
+  shellSize(relative?: string): { width: number; height: number };
 }
 
 export interface PluginOpts {
