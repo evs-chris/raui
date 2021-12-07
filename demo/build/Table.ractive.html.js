412a751c1ff7e604242961a392d15b55c65c58e8
Chris Reeves <evschris@gmail.com>
2021-12-07 17:03:54 -0500

    publish 0.9.15 and build demo

--- demo/build/Table.ractive.html.js
+++ demo/build/Table.ractive.html.js
@@ -822,6 +822,9 @@ System.register(['ractive', './chunk8.js', './chunk9.js', './chunk2.js', './chun
               col.label = (e.f || []).find(function (e) { return e.e === 'header'; });
               if (col.label) {
                 col.label = col.label.f;
+                col.title = attrs.find(function (a) { return a.n === 'label'; });
+                if (!col.title) { col.title = attrs.find(function (a) { return a.n === 'header'; }); }
+                if (col.title && col.title.f) { col.title = map(col.title); }
               } else {
                 col.label = attrs.find(function (a) { return a.n === 'label'; });
                 if (!col.label) { col.label = attrs.find(function (a) { return a.n === 'header'; }); }
@@ -977,7 +980,7 @@ System.register(['ractive', './chunk8.js', './chunk9.js', './chunk2.js', './chun
           { t: 4, n: 50, r: '~/fixedHeader', f: [{ t: 13, n: 'style-padding-right', f: [{ t: 2, r: '~/scrollOffset' }, 'px'] }] }
         ], f: [{ t: 7, e: 'div', f: columns.filter(function (c) { return c.hidden !== true; }).map(function (c, cidx) {
           c.attrsHP = c.attrs.filter(function (a) { return a.n !== 'title'; });
-          var div = { t: 7, e: 'div', f: [{ t: 7, e: 'div', f: [{ t: 16, r: ("~/columns." + cidx + ".label"), c: { r: '.' }, z: z }] }], m: [{ t: 13, n: 'title', f: c.label }, { t: 16, r: ("~/columns." + cidx + ".attrsHP"), z: z }] };
+          var div = { t: 7, e: 'div', f: [{ t: 7, e: 'div', f: [{ t: 16, r: ("~/columns." + cidx + ".label"), c: { r: '.' }, z: z }] }], m: [{ t: 13, n: 'title', f: c.title || c.label }, { t: 16, r: ("~/columns." + cidx + ".attrsHP"), z: z }] };
           if (c.type) { div.m.push({ t: 13, n: ("class-rtable-" + (c.type) + "-column") }); }
           if (c.filter || c.sort) { div.m.push({ t: 13, n: 'class-rtable-sortable' }, { t: 4, n: 53, r: ("~/columns." + cidx), f: [{ t: 70, n: ['click'], f: { r: ['@this', '.index', '@event'], s: sortKey } }] }); }
 
