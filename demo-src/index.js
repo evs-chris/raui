6b316a7323d7525fe048ae24525751efff8ed8b6
Chris Reeves <evschris@gmail.com>
2021-12-28 16:12:54 -0500

    demo: introduce more advanced debug helpers

--- demo-src/index.js
+++ demo-src/index.js
@@ -16,15 +16,35 @@ var el;
 document.addEventListener('click', ev => el = ev.target);
 document.addEventListener('focus', ev => el = ev.target);
 
-Object.defineProperty(window, 'C', {
-  get() {
-    return app.getContext(el);
-  }
-});
-
-Object.defineProperty(window, 'R', {
-  get() {
-    const res = window.C;
-    if (res) return res.ractive;
-  }
+Object.defineProperty(globalThis, 'R', {
+  value: new Proxy(() => ({}), {
+    apply(_obj, _e, args) {
+      if (args.length) {
+        let ctx;
+        if (typeof args[0] === 'object' && args[0] instanceof Node) ctx = Ractive.getContext(args.shift());
+        else ctx = Ractive.getContext(el);
+        if (!ctx) return;
+        if (typeof args[0] === 'string') {
+          if (args.length === 1) return ctx.get(args[0]);
+          else if (args.length === 2) return ctx.set(args[0], args[1]);
+        } else if (typeof args[0] === 'object') {
+          return ctx.set(args[0]);
+        }
+        return ctx;
+      } else {
+        return Ractive.getContext(el).get();
+      }
+    },
+    get(_obj, prop) {
+      const ctx = Ractive.getContext(el);
+      if (!ctx) return;
+      if (!(prop in ctx) && prop in ctx.ractive) {
+        const val = ctx.ractive[prop];
+        if (typeof val === 'function') return val.bind(ctx.ractive);
+        return val;
+      } else {
+        return ctx[prop];
+      }
+    },
+  }),
 });
